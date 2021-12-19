1e9e973d256edeefb299d5e8acd59e1f0a472221
M.Bashir <82314085+mohammed75296@users.noreply.github.com>
2021-12-19 04:33:40 -0800

    Add files via upload

---
+++ my_website/admin/includes/templets/foter.php
@@ -0,0 +1,12 @@
+
+        <div class = "foter">
+        
+        </div>
+        
+            <script src = "<?php echo $js ?>jquery-3.6.0.min.js"></script>
+            <script src = "<?php echo $js ?>bootstrap.js"></script>
+			<script src = "<?php echo $js ?>wow.min.js"></script>
+			<script>new WOW().init();</script>
+        </div> 
+    </body>    
+</html>
---
+++ my_website/admin/index.php
@@ -0,0 +1,7 @@
+<?php
+   $navbar = "";
+   include 'init.php';
+
+?>
+<?php include $tpl.'foter.php';?>
+<!-- https://kalas777.000webhostapp.com/ -->
